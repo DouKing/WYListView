3952f5f12af8be84781efbba2eca8f905eebb5f7
wuyikai <wuyikai@secoo.com>
2016-10-26 15:32:06 +0800

    MOD: 地区选择

--- WYListViewController/DetailViewController.swift
+++ WYListViewController/DetailViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    enum AddressType: Int {
+        case province, city, area
+    }
+    var sectionDataSource: [String?] = [nil]
+    var selectedRows: [Int?] = [nil, nil, nil]
+    
+    let name = "n"
+    let code = "c"
+
     lazy var dataSource: NSDictionary = {
         let path = Bundle.main.path(forResource: "test", ofType: "plist")
         let info = NSDictionary(contentsOfFile: path!)
@@ -30,7 +39,7 @@ class DetailViewController: UIViewController {
         let provinces = self.dataSource["areas"]
         return provinces! as! [String : [[String : String]]]
     }()
-
+    
     @IBOutlet weak var contentView: UIView!
     
     override func viewDidLoad() {
@@ -53,22 +62,100 @@ class DetailViewController: UIViewController {
 // MARK: - WYListViewDataSource -
 extension DetailViewController: WYListViewDataSource, WYListViewDelegate {
     func numberOfSections(in listView: WYListView) -> Int {
-        return 1
+        return self.sectionDataSource.count
     }
     
     func listView(_ listView: WYListView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        guard let type = AddressType(rawValue: section) else {
+            return 0
+        }
+        switch type {
+        case .province:
+            return self.provinces.count
+        case .city:
+            guard let index = self.selectedRows[AddressType.province.rawValue] else { return 0 }
+            guard let code = self.provinces[index][self.code] else { return 0 }
+            guard let cities = self.cities[code] else { return 0 }
+            return cities.count
+        case .area:
+            guard let index = self.selectedRows[AddressType.province.rawValue] else { return 0 }
+            guard let code = self.provinces[index][self.code] else { return 0 }
+            guard let cities = self.cities[code] else { return 0 }
+            
+            guard let idx = self.selectedRows[AddressType.city.rawValue] else { return 0 }
+            guard let c = cities[idx][self.code] else { return 0 }
+            guard let areas = self.areas[c] else { return 0 }
+            return areas.count
+        }
     }
     
     func listView(_ listView: WYListView, titleForSection section: Int) -> String? {
-        return "section:" + String(section)
+        return self.sectionDataSource[section]
     }
     
     func listView(_ listView: WYListView, titleForRowAtIndexPath indexPath: IndexPath) -> String? {
-        return "section:" + String(indexPath.section) + ", row:" + String(indexPath.row)
+        guard let type = AddressType(rawValue: indexPath.section) else {
+            return nil
+        }
+        switch type {
+        case .province:
+            return self.provinces[indexPath.row][self.name]
+        case .city:
+            guard let index = self.selectedRows[AddressType.province.rawValue] else { return nil }
+            guard let code = self.provinces[index][self.code] else { return nil }
+            guard let cities = self.cities[code] else { return nil }
+            return cities[indexPath.row][self.name]
+        case .area:
+            guard let index = self.selectedRows[AddressType.province.rawValue] else { return nil }
+            guard let code = self.provinces[index][self.code] else { return nil }
+            guard let cities = self.cities[code] else { return nil }
+            
+            guard let idx = self.selectedRows[AddressType.city.rawValue] else { return nil }
+            guard let c = cities[idx][self.code] else { return nil }
+            guard let areas = self.areas[c] else { return nil }
+            return areas[indexPath.row][self.name]
+        }
     }
     
     func listView(_ listView: WYListView, didSelectRowAtIndexPath indexPath: IndexPath) {
-        print("did select (\(indexPath.section), \(indexPath.row))")
+        let row = self.selectedRows[indexPath.section]
+        
+        self.selectedRows[indexPath.section] = indexPath.row
+        self.sectionDataSource[indexPath.section] = self.listView(listView, titleForRowAtIndexPath: indexPath)
+        
+        if let selectedRow = row {
+            if selectedRow != indexPath.row {
+                
+            }
+        }
+        
+        if let selectedRow = row, selectedRow == indexPath.row {
+        } else {
+            self.selectedRows.remove(after: indexPath.section)
+            self.sectionDataSource.remove(after: indexPath.section)
+            if indexPath.section < AddressType.area.rawValue {
+                self.sectionDataSource.append(nil)
+                for _ in (indexPath.section + 1)...AddressType.area.rawValue {
+                    self.selectedRows.append(nil)
+                }
+            }
+        }
+        
+        var selectedIndexPaths: [IndexPath] = []
+        for (idx, row) in self.selectedRows.enumerated() {
+            if row != nil {
+                selectedIndexPaths.append(IndexPath(row: row!, section: idx))
+            }
+        }
+        listView.reloadData(selectRowsAtIndexPaths: selectedIndexPaths)
+        listView.scroll(to: min(indexPath.section + 1, AddressType.area.rawValue), animated: true, after: 0.3)
+    }
+}
+
+extension Array {
+    mutating func remove(after index: Int) {
+        while self.count > index + 1 {
+            self.removeLast()
+        }
     }
 }
